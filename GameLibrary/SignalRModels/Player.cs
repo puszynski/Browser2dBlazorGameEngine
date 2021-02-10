f3bdd280cd3d24048e92632338f007e0e4a9bfff
jakub.puszynski <jakub.puszynski@efitness.com>
2021-02-10 19:12:12 +0100

    fff

---
+++ GameLibrary/SignalRModels/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameLibrary.Enums;
+
+namespace GameLibrary.SignalRModels
+{
+    public class Player
+    {
+        public string ID { get; set; } 
+        public string ImgSrc { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string MapID { get; set; }     
+        public bool Walking { get; set; }
+        public EDirection Facing { get; set; }
+    }
+}
